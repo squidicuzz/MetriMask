25d8f6758916cd25ad39c2b222b92024843b2bad
nibbles83 <hardke01@yahoo.co.uk>
2021-09-01 13:19:21 +0100

    Allow editing of gas fee for signed tx calls

--- static/js/sign-tx.js
+++ static/js/sign-tx.js
@@ -6,17 +6,25 @@ const updateFields = () => {
   const to = args[0];
   const amount = args[2] || 0;
   const gasLimit = args[3] || 250000;
-  const gasPrice = args[4] ? Number(args[4]) : 5000;
+  const gasPrice = args[4] ? gasConvert(Number(args[4])) : 5000;
   const maxTxFee = Math.round(gasLimit * gasPrice * 1000) / 1e11;
 
   document.getElementById('from-field').innerText = fromAddress;
   document.getElementById('to-field').innerText = to;
   document.getElementById('amount-field').innerText = amount;
   document.getElementById('gas-limit-field').innerText = gasLimit;
-  document.getElementById('gas-price-field').innerText = gasPrice;
+  document.getElementById('gas-price-field').value = gasPrice;
   document.getElementById('max-tx-fee-field').innerText = maxTxFee;
   document.getElementById('raw-tx-field').innerText = JSON.stringify(request);
 };
+
+function gasConvert(amount) {
+  if(amount % 1 != 0) {
+    request.args[4] = amount * 1e8;
+    return amount * 1e8;
+  }
+  return amount;
+}
 
 const extractReqParams = () => {
   const urlParams = window.location.search.substr(1).split('&');
@@ -57,4 +65,8 @@ window.onload = () => {
   extractReqParams();
   document.getElementById('button-confirm').addEventListener('click', confirmTransaction);
   document.getElementById('button-cancel').addEventListener('click', cancelTransaction);
+  document.getElementById('gas-price-field').addEventListener('change', (res) => {
+    request.args[4] = res.target.value ? parseInt(res.target.value) : 5000
+    updateFields();
+  });
 }
